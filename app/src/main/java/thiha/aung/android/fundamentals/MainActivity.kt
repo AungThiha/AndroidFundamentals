7768064dad75d1640c07b2e58b5d61e2d307a9e6
Aung Thiha <mr.aungthiha@gmail.com>
2020-11-17 22:16:53 +0700

    first commit

---
+++ app/src/main/java/thiha/aung/android/fundamentals/MainActivity.kt
@@ -0,0 +1,11 @@
+package thiha.aung.android.fundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
